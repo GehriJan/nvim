1c72ca0f5e5eeecfd0a55365e66fe1e2a24ad3c4
Gehring Jannis (VM/EYV4-LA) <jannis.gehring@bosch.com>
2026-08-14 16:05:18 +0200

    Feature: add telekasten

---
+++ lua/plugins/telekasten.lua
@@ -0,0 +1,26 @@
+return {
+  "renerocksai/telekasten.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    home = vim.fn.expand("~/src/notes"), -- Change this to your notes directory
+    -- Add any other custom settings here!
+    -- external_link_follow = true,
+    templates = vim.fn.expand("~/src/notes/templates"),
+    -- Associate default templates for automatic note creation
+    template_new_note = vim.fn.expand("~/src/notes/templates/new_note.md"),
+    template_new_daily = vim.fn.expand("~/src/notes/templates/daily_note.md"),
+  },
+  keys = {
+    -- Launch panel (the command palette) if nothing is typed after <leader>z
+    { "<leader>z", "<cmd>Telekasten panel<cr>", desc = "Telekasten Panel" },
+    -- Most used functions
+    { "<leader>zf", "<cmd>Telekasten find_notes<cr>", desc = "Find Notes" },
+    { "<leader>zg", "<cmd>Telekasten search_notes<cr>", desc = "Search Notes (Grep)" },
+    { "<leader>zd", "<cmd>Telekasten goto_today<cr>", desc = "Go to Today's Note" },
+    { "<leader>zz", "<cmd>Telekasten follow_link<cr>", desc = "Follow Link" },
+    { "<leader>zn", "<cmd>Telekasten new_note<cr>", desc = "Create New Note" },
+    { "<leader>zc", "<cmd>Telekasten show_calendar<cr>", desc = "Show Calendar" },
+    { "<leader>zb", "<cmd>Telekasten show_backlinks<cr>", desc = "Show Backlinks" },
+    { "<leader>zI", "<cmd>Telekasten insert_img_link<cr>", desc = "Insert Image Link" },
+  },
+}
