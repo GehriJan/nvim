42630e3177485d0253da1225a0f468ab10390158
Gehring Jannis (VM/EYV4-LA) <jannis.gehring@bosch.com>
2026-08-18 16:07:05 +0200

    Feature: add custom keymaps and layout adaptations
    - Add German keyboard layout shortcuts (ä, ö, Ä)
    
    - Add Ctrl-y shortcut for copying to the system clipboard
    
    - Add <leader>rw mapping to replace the word under cursor
    
    - Add <leader><leader> shortcut to toggle back to last active buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,20 @@ vim.keymap.set("n", "H", ":bprevious<CR>", { silent = true, desc = "Previous Buf
 vim.keymap.set("n", "L", ":bnext<CR>", { silent = true, desc = "Next Buffer" })
 -- vim.keymap.set("n", ":<Leader>os", require("mini.starter").open, { desc = "Open mini starter" })
 
+-- German Layout Adaptations
+vim.keymap.set("n", "ä", "<C-]>", { desc = "Jump to definition" })
+vim.keymap.set("n", "ö", "<C-[>", { desc = "Escape Normal Mode" })
+vim.keymap.set("n", "Ä", "<C-o>", { desc = "Jump backward" })
+
+-- Clipboard Shortcut
+vim.keymap.set("v", "<C-y>", '"+y', { desc = "Copy to system clipboard" })
+
+-- Search/Replace Word Under Cursor
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/]], { desc = "Replace word under cursor" })
+
+-- Fast last active buffer toggle
+vim.keymap.set("n", "<leader><leader>", "<cmd>b#<cr>", { desc = "Toggle last active buffer" })
+
 -- Insert Mode
 vim.keymap.set("i", "kj", "<ESC>", { silent = true, desc = "Escape" })
 
